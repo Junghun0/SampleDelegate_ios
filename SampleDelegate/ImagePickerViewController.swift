e2025ee219bfddf5dc9dccc1f173b4e7f6960701
parkjunghun <go9018@naver.com>
2019-01-15 16:56:22 +0900

    [2019.1.15] extension 을 사용한 코드분리 /  //MARK:- 를 통한 점프바 확인

--- SampleDelegate/ImagePickerViewController.swift
+++ SampleDelegate/ImagePickerViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class ImagePickerViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+class ImagePickerViewController : UIViewController{
 
     @IBOutlet weak var imgView: UIImageView!
     
@@ -17,25 +17,6 @@ class ImagePickerViewController : UIViewController, UIImagePickerControllerDeleg
         super.viewDidLoad()
     }
     
-    //이미지 피커에서 이미지를 선택하지 않고 취소했을 때 호출되는 메소드
-    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
-        picker.dismiss(animated: false, completion: { () in // image picker controller 가 완전히 종료된후 실행될 부분
-            //알림창 호출
-            let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: nil))
-            self.present(alert,animated: false)
-        })
-        
-
-    }
-    //이미지 피커에서 이미지를 선택했을 때 호출되는 메소드
-    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
-        //이미지 피커 컨트롤러 닫기
-        picker.dismiss(animated: false, completion: { () in
-            let img = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
-            self.imgView.image = img
-        })
-    }
     @IBAction func pick(_ sender: Any) {
         //이미지 피커 컨트롤러 인스턴스 생성
         let picker = UIImagePickerController()
@@ -47,5 +28,28 @@ class ImagePickerViewController : UIViewController, UIImagePickerControllerDeleg
         
         //이미지 피커 컨트롤러 실행
         self.present(picker, animated: false)
+    }
+}
+
+// MARK:- 이미지피커컨트롤러 메소드
+extension ImagePickerViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    //이미지 피커에서 이미지를 선택하지 않고 취소했을 때 호출되는 메소드
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        picker.dismiss(animated: false, completion: { () in // image picker controller 가 완전히 종료된후 실행될 부분
+            //알림창 호출
+            let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: nil))
+            self.present(alert,animated: false)
+        })
+    }
+    
+    //이미지 피커에서 이미지를 선택했을 때 호출되는 메소드
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        //이미지 피커 컨트롤러 닫기
+        picker.dismiss(animated: false, completion: { () in
+            let img = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+            self.imgView.image = img
+        })
     }
 }
