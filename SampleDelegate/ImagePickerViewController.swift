33b946b5e269fb4e5c309c1f7e0ccc2ecb97286f
parkjunghun <go9018@naver.com>
2019-01-15 16:47:48 +0900

    [2019.1.15] Delegate - UIImagePickerControllerDelegate, UINavigationControllerDelegate 사용해 앨범이미지 가져오기

--- SampleDelegate/ImagePickerViewController.swift
+++ SampleDelegate/ImagePickerViewController.swift
@@ -9,10 +9,43 @@
 import Foundation
 import UIKit
 
-class ImagePickerViewController : UIViewController{
+class ImagePickerViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var imgView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    //이미지 피커에서 이미지를 선택하지 않고 취소했을 때 호출되는 메소드
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        picker.dismiss(animated: false, completion: { () in // image picker controller 가 완전히 종료된후 실행될 부분
+            //알림창 호출
+            let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: nil))
+            self.present(alert,animated: false)
+        })
+        
+
+    }
+    //이미지 피커에서 이미지를 선택했을 때 호출되는 메소드
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        //이미지 피커 컨트롤러 닫기
+        picker.dismiss(animated: false, completion: { () in
+            let img = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+            self.imgView.image = img
+        })
+    }
+    @IBAction func pick(_ sender: Any) {
+        //이미지 피커 컨트롤러 인스턴스 생성
+        let picker = UIImagePickerController()
+        //핸드폰으로 앨범에 접근하려면 Info.plist 파일에서 privacy - photo library usage description 키를 추가하고 yes 로 변경해주어야함(권한처리)
+        picker.sourceType = .photoLibrary //이미지 소스로 사진 라이브러리 선택
+        picker.allowsEditing = true // 이미지 편집 기능 On
+        
+        picker.delegate = self
+        
+        //이미지 피커 컨트롤러 실행
+        self.present(picker, animated: false)
     }
 }
